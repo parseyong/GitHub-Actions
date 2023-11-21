3d4fd8548d212f1ea40c841473a59bd917c30221
parseyong <psy217300@naver.com>
2023-11-22 01:35:52 +0900

    Test

--- src/test/java/com/example/actions/ActionsApplicationTests.java
+++ src/test/java/com/example/actions/ActionsApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.actions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ActionsApplicationTests {
 
     @Test
